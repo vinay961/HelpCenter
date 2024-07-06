4e4d09f2af2261237c1375c4162be65042129f1d
vinay961 <vinay100rai@gmail.com>
2024-07-06 22:11:59 +0530

    .

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -5,7 +5,7 @@ import cors from 'cors'
 const app = express();
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://helpcenter-frontend.onrender.com',
     credentials: true 
 }));
 app.use(express.json({limit:"16kb"}));
