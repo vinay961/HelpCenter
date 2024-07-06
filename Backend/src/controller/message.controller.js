a9e499da81aa5d8b2f72bccd08ba2c0dd347c6e0
vinay961 <vinay100rai@gmail.com>
2024-07-06 21:36:38 +0530

    .

--- Backend/src/controller/message.controller.js
+++ Backend/src/controller/message.controller.js
@@ -1,12 +1,11 @@
 import {asyncHandler} from '../utils/asyncHandler.js'
 import {ApiError} from '../utils/ApiError.js'
-import { ApiResponse } from '../utils/ApiResponse.js'
+import { ApiResponse } from '../utils/ApiResponse.js' 
 
 const messageHandle = asyncHandler(async(req,res) => {
     const { message,roomBooked } = req.body
     if(!message){
         throw new ApiError(400,"Message typing error")
     }
-
-
+    
 })
